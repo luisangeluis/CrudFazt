de0f3b139515aece301c3685d80182b21fd91268
luisangeluis <luisangeluis@gmail.com>
2022-08-03 17:08:33 -0500

    Configured bd

--- src/database.js
+++ src/database.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 
 (async () => {
   console.log('hola');
+  const pass = 'Nxtlxer00';
   try {
     const db = await mongoose.connect(
-      'mongodb+srv://luisangeluis:Vqv5bywtyUR1h87E@cluster0.oaq1txq.mongodb.net/retryWrites=true&w=majority'
+      // 'mongodb+srv://luisangeluis:Nxtlxer00@cluster0.oaq1txq.mongodb.net/?retryWrites=true&w=majority'
+      `mongodb+srv://luisangeluis:${pass}@cluster0.oaq1txq.mongodb.net/?retryWrites=true&w=majority`
+      // 'a'
     );
-    console.log('Db connect to', db);
+    // console.log('Db connect to', db);
     console.log('adios');
   } catch (error) {
     console.log(error);
